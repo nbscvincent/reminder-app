67f21bb1f6023ad615b01597205b94afe89b23f6
jsimangcastudent <147906200+jsimangcastudent@users.noreply.github.com>
2023-10-20 04:58:54 +0800

    Registration Screen
    Registration Screen
    Add new image in drawable

---
+++ app/src/main/java/com/nbscollege_jenjosh/schdulix/Registration.kt
@@ -0,0 +1,166 @@
+package com.nbscollege_jenjosh.schdulix
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RegistrationScreen(modifier: Modifier = Modifier) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var lastName by remember { mutableStateOf("") }
+    var firstName by remember { mutableStateOf("") }
+
+    Scaffold (
+    ){ innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .fillMaxWidth()
+                .background(Color.White),
+            verticalArrangement = Arrangement.spacedBy(0.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.schdulix_logo),
+                contentDescription = "Schdulix",
+                modifier = Modifier
+                    .height(200.dp)
+                    .fillMaxWidth()
+                    .padding(start = 25.dp, end = 25.dp)
+            )
+            Text(
+                text = "Welcome",
+                fontWeight = FontWeight.Bold,
+                fontSize = 30.sp,
+                color = Color(0xFF6562DF),
+            )
+            Spacer(modifier = Modifier.height(50.dp))
+            OutlinedTextField(
+                value = username,
+                onValueChange = { username = it },
+                shape = RoundedCornerShape(10.dp),
+                trailingIcon = {
+                    Icon(imageVector = Icons.Default.Person, contentDescription = "Username")
+                },
+                placeholder = { Text(text = "Username") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 25.dp, end = 25.dp, top = 0.dp, bottom = 0.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    textColor = Color.Black
+                )
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                shape = RoundedCornerShape(10.dp),
+                trailingIcon = {
+                    Icon(imageVector = Icons.Default.Lock, contentDescription = "Password")
+                },
+                placeholder = { Text(text = "Strong Password") },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 25.dp, end = 25.dp, top = 0.dp, bottom = 0.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    textColor = Color.Black
+                ),
+                visualTransformation = PasswordVisualTransformation()
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            OutlinedTextField(
+                value = firstName,
+                onValueChange = { firstName = it },
+                shape = RoundedCornerShape(10.dp),
+                trailingIcon = {
+                    Icon(imageVector = Icons.Default.Person, contentDescription = "First Name")
+                },
+                placeholder = { Text(text = "First Name") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 25.dp, end = 25.dp, top = 0.dp, bottom = 0.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    textColor = Color.Black
+                )
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            OutlinedTextField(
+                value = lastName,
+                onValueChange = { lastName = it },
+                shape = RoundedCornerShape(10.dp),
+                trailingIcon = {
+                    Icon(imageVector = Icons.Default.Person, contentDescription = "Last Name")
+                },
+                placeholder = { Text(text = "Last Name") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 25.dp, end = 25.dp, top = 0.dp, bottom = 0.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    textColor = Color.Black
+                )
+            )
+            Spacer(modifier = Modifier.height(15.dp))
+            Button(
+                onClick = { },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 25.dp, end = 25.dp, top = 0.dp, bottom = 0.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF6562DF)
+                ),
+                shape = RoundedCornerShape(5.dp),
+            ) {
+                Text(
+                    text = "SIGN UP",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = Color.White,
+                )
+            }
+        }
+    }
+}
