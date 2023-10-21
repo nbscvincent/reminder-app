b0146766a5ead6214b92d22fc1e8c09f6b7cff2a
jnprjbll00 <jjabillo2021@student.nbscollege.edu.ph>
2023-10-21 12:38:40 +0800

    Update loginscreen
    update loginscreen

--- app/src/main/java/com/nbscollege_jenjosh/schdulix/LoginScreen.kt
+++ app/src/main/java/com/nbscollege_jenjosh/schdulix/LoginScreen.kt
@@ -44,145 +44,143 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.nbscollege_jenjosh.schdulix.ui.theme.SchdulixTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreen() {
+    var email by remember { mutableStateOf("") }
+    val password by remember { mutableStateOf("") }
+    val isChecked = remember { mutableStateOf(true) }
 
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    fun LoginScreen() {
-        var email by remember { mutableStateOf("") }
-        var password by remember { mutableStateOf("") }
-        val isChecked = remember { mutableStateOf(true) }
-
-        Scaffold(
-            bottomBar = {
-                BottomAppBar(
-                    modifier = Modifier.height(120.dp),
-                    containerColor = Color.White
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .padding(start = 25.dp, end = 25.dp, top = 0.dp, bottom = 0.dp)
-                            .fillMaxSize()
-                            .fillMaxWidth()
-                            .background(Color.White),
-                        verticalArrangement = Arrangement.spacedBy(10.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                    ) {
-                        Button(
-                            onClick = { },
-                            modifier = Modifier
-                                .fillMaxWidth(),
-                            colors = ButtonDefaults.buttonColors(
-                                containerColor = Color.Red
-                            ),
-                            shape = RoundedCornerShape(10.dp),
-                        ) {
-                            Text(
-                                text = "Next >",
-                                fontWeight = FontWeight.Bold,
-                                fontSize = 20.sp,
-                                color = Color.White,
-                            )
-                        }
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth(),
-                            horizontalArrangement = Arrangement.Center
-                        ) {
-                            Text(
-                                text = "New Member? ",
-                                fontWeight = FontWeight.Normal,
-                                fontSize = 15.sp,
-                                color = Color.Black,
-                            )
-                            Text(
-                                text = "Register now",
-                                fontWeight = FontWeight.Bold,
-                                fontSize = 15.sp,
-                                color = Color.Red,
-                            )
-                        }
-                    }
-                }
-            }
-        ) { innerPadding ->
-            Column(
-                modifier = Modifier
-                    .padding(innerPadding)
-                    .fillMaxSize()
-                    .fillMaxWidth()
-                    .background(Color.White),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(0.dp)
+    Scaffold(
+        bottomBar = {
+            BottomAppBar(
+                modifier = Modifier.height(120.dp),
+                containerColor = Color.White
             ) {
-                Spacer(modifier = Modifier.height(40.dp))
-                Image(
-                    painter = painterResource(id = R.drawable.img_nbs),
-                    contentDescription = "nbs",
-                    modifier = Modifier.size(200.dp)
-                )
-                Text(
-                    text = "Welcome Back!",
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 30.sp,
-                    color = Color.Black
-                )
-                Text(
-                    text = "sign in to access your account",
-                    fontWeight = FontWeight.Normal,
-                    fontSize = 15.sp,
-                    color = Color.Black
-                )
-                Spacer(modifier = Modifier.height(50.dp))
-                OutlinedTextField(
+                Column(
                     modifier = Modifier
+                        .padding(start = 25.dp, end = 25.dp, top = 0.dp, bottom = 0.dp)
+                        .fillMaxSize()
                         .fillMaxWidth()
-                        .padding(start = 25.dp, end = 25.dp),
-                    value = email,
-                    shape = RoundedCornerShape(10.dp),
-                    onValueChange = { email = it },
-                    label = { Text(text = "Enter your email") },
-                    trailingIcon = {
-                        Icon(imageVector = Icons.Default.Email, contentDescription = "Email")
-                    }
-                )
-                Spacer(modifier = Modifier.height(15.dp))
-                OutlinedTextField(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 25.dp, end = 25.dp),
-                    value = password,
-                    shape = RoundedCornerShape(10.dp),
-                    onValueChange = {},
-                    label = { Text(text = "Password") },
-                    trailingIcon = {
-                        Icon(imageVector = Icons.Default.Lock, contentDescription = "Password")
-                    }
-                )
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 25.dp, end = 25.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween
+                        .background(Color.White),
+                    verticalArrangement = Arrangement.spacedBy(10.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Checkbox(
-                            checked = isChecked.value,
-                            onCheckedChange = { isChecked.value = it },
-                            enabled = true,
-                            colors = CheckboxDefaults.colors(Color.Red)
+                    Button(
+                        onClick = { },
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Blue
+                        ),
+                        shape = RoundedCornerShape(10.dp),
+                    ) {
+                        Text(
+                            text = "Log In",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 20.sp,
+                            color = Color.White,
+                        )
+                    }
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = "Don't have account? ",
+                            fontWeight = FontWeight.Normal,
+                            fontSize = 15.sp,
+                            color = Color.Black,
                         )
                         Text(
-                            text = "Remember me",
-                            color = Color.Black
+                            text = "Sign Up",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 15.sp,
+                            color = Color.Red,
                         )
                     }
-                    Text(
-                        color = Color.Red,
-                        text = "Forgot password?"
-                    )
                 }
             }
         }
-
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .fillMaxWidth()
+                .background(Color.White),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(0.dp)
+        ) {
+            Spacer(modifier = Modifier.height(40.dp))
+            Image(
+                painter = painterResource(id = R.drawable.img_nbs),
+                contentDescription = "nbs",
+                modifier = Modifier.size(200.dp)
+            )
+            Text(
+                text = "Welcome to Schdulix!",
+                fontWeight = FontWeight.Bold,
+                fontSize = 30.sp,
+                color = Color.Black
+            )
+            Text(
+                text = "Sign in to access your account",
+                fontWeight = FontWeight.Normal,
+                fontSize = 15.sp,
+                color = Color.Black
+            )
+            Spacer(modifier = Modifier.height(50.dp))
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 25.dp, end = 25.dp),
+                value = email,
+                shape = RoundedCornerShape(10.dp),
+                onValueChange = { email = it },
+                label = { Text(text = "Enter your email") },
+                trailingIcon = {
+                    Icon(imageVector = Icons.Default.Email, contentDescription = "Email")
+                }
+            )
+            Spacer(modifier = Modifier.height(15.dp))
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 25.dp, end = 25.dp),
+                value = password,
+                shape = RoundedCornerShape(10.dp),
+                onValueChange = {},
+                label = { Text(text = "Password") },
+                trailingIcon = {
+                    Icon(imageVector = Icons.Default.Lock, contentDescription = "Password")
+                }
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 25.dp, end = 25.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Checkbox(
+                        checked = isChecked.value,
+                        onCheckedChange = { isChecked.value = it },
+                        enabled = true,
+                        colors = CheckboxDefaults.colors(Color.Blue)
+                    )
+                    Text(
+                        text = "Remember me",
+                        color = Color.Black
+                    )
+                }
+                Text(
+                    color = Color.Red,
+                    text = "Forgot password?"
+                )
+            }
+        }
+    }
 }
