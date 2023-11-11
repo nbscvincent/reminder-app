2fa52018dc0c48648438557f2a8320ad3b69f49c
jysaac19 <147906042+jysaac19@users.noreply.github.com>
2023-11-11 17:10:34 +0800

    11/11/23 Commit Progress
    Print user input data
    navigation from 'sign up' to log in
    draft for pop up message after clicking sign up

--- app/src/main/java/com/nbscollege_jenjosh/schdulix/Registration.kt
+++ app/src/main/java/com/nbscollege_jenjosh/schdulix/Registration.kt
@@ -1,10 +1,7 @@
 package com.nbscollege_jenjosh.schdulix
 
-import android.annotation.SuppressLint
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -12,13 +9,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -26,6 +24,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -35,6 +34,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -42,7 +42,7 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.NavHostController
+import com.nbscollege_jenjosh.schdulix.navigation.routes.MainScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -148,7 +148,16 @@ fun RegistrationScreen( navController: NavController ) {
             Spacer(modifier = Modifier.height(15.dp))
             Button(
                 onClick = {
-                        print("usercreated")},
+                        Register(username, password, firstName, lastName)
+                    //navController.navigate(MainScreen.Login.name)
+                    /*AlertDialogExample(
+                        onDismissRequest = { /*TODO*/ },
+                        onConfirmation = { /*TODO*/ },
+                        dialogTitle = ,
+                        dialogText = ,
+                        icon =
+                    )*/
+                },
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(start = 25.dp, end = 25.dp, top = 0.dp, bottom = 0.dp),
@@ -169,8 +178,76 @@ fun RegistrationScreen( navController: NavController ) {
 }
 fun Register(username: String, password: String, firstName: String, lastName: String) {
     val user = UserProfile(username, password, firstName, lastName)
-    print("usercreated $user")
+    println("usercreated $user")
 }
 data class UserProfile(val username: String, val password: String, val firstName: String, val lastName: String) {
-
 }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AlertDialogExample(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    dialogTitle: String,
+    dialogText: String,
+    icon: ImageVector,
+) {
+    AlertDialog(
+        icon = {
+            Icon(icon, contentDescription = "Example Icon")
+        },
+        title = {
+            Text(text = dialogTitle)
+        },
+        text = {
+            Text(text = dialogText)
+        },
+        onDismissRequest = {
+            onDismissRequest()
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onConfirmation()
+                }
+            ) {
+                Text("Confirm")
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    onDismissRequest()
+                }
+            ) {
+                Text("Dismiss")
+            }
+        }
+    )
+}
+
+@Composable
+fun DialogExamples() {
+    // ...
+    val openAlertDialog = remember { mutableStateOf(false) }
+
+    // ...
+    when {
+        // ...
+        openAlertDialog.value -> {
+            AlertDialogExample(
+                onDismissRequest = { openAlertDialog.value = false },
+                onConfirmation = {
+                    openAlertDialog.value = false
+                    println("Confirmation registered") // Add logic here to handle confirmation.
+                },
+                dialogTitle = "Alert dialog example",
+                dialogText = "This is an example of an alert dialog with buttons.",
+                icon = Icons.Default.Info
+            )
+        }
+    }
+}
+
+
+
+
